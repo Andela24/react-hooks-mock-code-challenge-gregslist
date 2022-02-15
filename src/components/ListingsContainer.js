ce048f36a5cd409911140eb42de582700f4c097c
Andela Stevanovic <andjela.s166@gmail.com>
2022-02-15 09:08:52 -0500

    commit tree e7936d83bef4d28a575e985332a03e686ae73d10

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,17 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+import React from "react";
+import ListingCard from "./ListingCard";
+
+function ListingsContainer( { itemsArray, deleteItemsFromState }) {
+
+  //iretate through array of objects and transfor to an array of components
+const cardComponents = itemsArray.map(item => <ListingCard key={item.id} {...item} deleteItemsFromState= {deleteItemsFromState}/>)
+
   return (
     <main>
       <ul className="cards">
         {/* use the ListingCard component to display listings */}
+        {cardComponents}
       </ul>
     </main>
   );
